d444b0d2d87b6fc4afa579bf45f00c73af343df5
Dominic <91763333+D0m-inic@users.noreply.github.com>
2022-12-17 16:19:15 +0000

    Time: 3 ms (48.21%), Space: 42.8 MB (8.10%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,9 +1,6 @@
 class Solution {
     public String decodeString(String s) {
-        
-        System.out.println(s);
         if (s == "") { return "";}
-        if (s.length() == 1) { return s; }
         if (Character.isAlphabetic(s.charAt(0))) { return s.substring(0,1) + decodeString(s.substring(1)); }
         
         int seperate = 0;
@@ -30,7 +27,6 @@ class Solution {
                 break;
             }
         }
-        System.out.println("HI" + s.substring(end+1));
         String restOfString = decodeString(s.substring(end+1));
         String repeatedString = decodeString(s.substring(start,end));
         String result = "";
