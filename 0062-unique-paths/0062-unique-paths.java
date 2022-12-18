05585ebd0ba23a99566c2567d428ec4c09563917
Dominic <91763333+D0m-inic@users.noreply.github.com>
2022-12-18 20:43:49 +0000

    Time: 0 ms (100.00%), Space: 39 MB (92.99%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -5,26 +5,86 @@ class Solution {
         for (int i=0; i<m; i++) {
             for (int j=0; j<n; j++) {
                 
-                if (i==0 && j==0) {
+                if (i == 0 && j == 0) {
                     dp[0][0] = 1;
                     continue;
                 }
-                
-                int leftSquareVal=0;
-                int upperSquareVal=0;
-                if (!((i-1)<0))
-                    leftSquareVal = dp[i-1][j];
-                
-                if (!((j-1)<0))
-                    upperSquareVal = dp[i][j-1];
-                
-                if (i-1<0)
-                    leftSquareVal = 0;
-                if (j-1<0)
-                    upperSquareVal = 0;
-                dp[i][j] = leftSquareVal + upperSquareVal;
+                int up = i-1>=0 ? dp[i-1][j]: 0 ;
+                int left = j-1>=0 ? dp[i][j-1]: 0 ;
+                dp[i][j] = up + left;
             }
         }
         return dp[m-1][n-1];
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int uniquePaths(int m, int n) {
+//         int[][] dp = new int[m][n];
+        
+//         for (int i=0; i<m; i++) {
+//             for (int j=0; j<n; j++) {
+                
+//                 if (i==0 && j==0) {
+//                     dp[0][0] = 1;
+//                     continue;
+//                 }
+                
+//                 int leftSquareVal=0;
+//                 int upperSquareVal=0;
+//                 if (!((i-1)<0))
+//                     leftSquareVal = dp[i-1][j];
+                
+//                 if (!((j-1)<0))
+//                     upperSquareVal = dp[i][j-1];
+                
+//                 if (i-1<0)
+//                     leftSquareVal = 0;
+//                 if (j-1<0)
+//                     upperSquareVal = 0;
+//                 dp[i][j] = leftSquareVal + upperSquareVal;
+//             }
+//         }
+//         return dp[m-1][n-1];
+//     }
+// }
