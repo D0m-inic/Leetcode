68020968b960010fa62e00139227e03fcc8b0f64
Dominic <91763333+D0m-inic@users.noreply.github.com>
2022-12-18 02:00:04 +0000

    Time: 0 ms (100.00%), Space: 41.7 MB (14.68%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int rob(int[] nums) {
+        int rob1 =0;
+        int rob2=0;
+        
+        for (int i=0; i<nums.length; i++) {
+            int tmp = Math.max(nums[i]+rob1, rob2);
+            rob1=rob2;
+            rob2=tmp;
+        }
+        return rob2;
+    }
+}
